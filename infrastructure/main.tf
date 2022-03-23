e8fdbb7a87144c27365a81439b8993bbae232913
cshiwarale <chetan.hiwarale@hmcts.net>
2022-03-23 13:38:49 +0000

    Remove redis configs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,21 +6,6 @@ locals {
   vaultName = "${var.product}-${var.env}"
 }
 
-data "azurerm_subnet" "core_infra_redis_subnet" {
-  name                 = "core-infra-subnet-1-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name = "core-infra-${var.env}"
-}
-
-module "fis-bulk-scan-api-session-storage" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = "${var.product}-${var.component}-session-storage"
-  location = var.location
-  env      = var.env
-  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
-  common_tags  = var.common_tags
-}
-
 data "azurerm_key_vault" "fis_key_vault" {
   name = local.vaultName
   resource_group_name = "${var.raw_product}-${var.env}"
