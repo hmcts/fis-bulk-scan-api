c0b4637cabc093518c064e522eb03dc60580f7da
Adetola Opanuga <2109031@cognizant.com>
2022-04-26 15:02:55 +0100

    Added testdata and updated more mandatory fields

--- src/test/java/uk/gov/hmcts/reform/bulkscan/utils/TestDataUtil.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/utils/TestDataUtil.java
@@ -26,4 +26,57 @@ public final class TestDataUtil {
 
         return Arrays.asList(ocrDataAddressField, ocrDataFirstNameField, ocrDataLastNameField);
     }
+
+    public static List<OcrDataField> buildFL403ValidationRequest() {
+
+        OcrDataField ocrDataApplicantFirstName = new OcrDataField();
+        ocrDataApplicantFirstName.setName("applicant_firstName");
+        ocrDataApplicantFirstName.setValue("firstName");
+
+        OcrDataField ocrDataApplicantLastName = new OcrDataField();
+        ocrDataApplicantLastName.setName("applicant_lastName");
+        ocrDataApplicantLastName.setValue("lastName");
+
+        OcrDataField ocrDataApplicantAddress = new OcrDataField();
+        ocrDataApplicantAddress.setName("applicant_address");
+        ocrDataApplicantAddress.setValue("Address in London");
+
+        OcrDataField ocrDataApplicantPostcode = new OcrDataField();
+        ocrDataApplicantPostcode.setName("applicant_postcode");
+        ocrDataApplicantPostcode.setValue("SW1 2EX");
+
+        OcrDataField ocrDataApplicantTelephoneField = new OcrDataField();
+        ocrDataApplicantTelephoneField.setName("applicant_telephone_number");
+
+        OcrDataField ocrDataApplicantEmailField = new OcrDataField();
+        ocrDataApplicantEmailField.setName("applicant_email");
+
+        OcrDataField ocrDataFirstNameField = new OcrDataField();
+        ocrDataFirstNameField.setName("solicitor_name");
+        ocrDataFirstNameField.setValue("solicitor_name_value");
+
+        OcrDataField ocrDataSolicitorAddressField = new OcrDataField();
+        ocrDataSolicitorAddressField.setName("solicitor_address");
+
+        OcrDataField ocrDataSolicitorTelephoneField = new OcrDataField();
+        ocrDataSolicitorTelephoneField.setName("solicitor_telephone_number");
+        ocrDataSolicitorTelephoneField.setValue("DUMMY");
+
+        OcrDataField ocrDataSolicitorDxField = new OcrDataField();
+        ocrDataSolicitorDxField.setName("solicitor_fax_number");
+        ocrDataSolicitorDxField.setValue("123455");
+
+        return Arrays.asList(ocrDataApplicantFirstName,
+                             ocrDataApplicantLastName,
+                             ocrDataApplicantAddress,
+                             ocrDataApplicantPostcode,
+                             ocrDataApplicantTelephoneField,
+                             ocrDataApplicantEmailField,
+                             ocrDataSolicitorAddressField,
+                             ocrDataFirstNameField,
+                             ocrDataSolicitorTelephoneField,
+                             ocrDataSolicitorDxField
+        );
+    }
+
 }
