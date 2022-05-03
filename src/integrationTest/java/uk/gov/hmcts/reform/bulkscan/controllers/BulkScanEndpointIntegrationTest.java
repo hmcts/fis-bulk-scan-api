05200b91ac306214186268ea0ad6fe01db75851f
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-05-03 09:40:30 +0100

    feat: implement validations for forms A60, C51 and C63(ISDB-147)

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/controllers/BulkScanEndpointIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/controllers/BulkScanEndpointIntegrationTest.java
@@ -13,6 +13,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.bulkscan.utils.Constants.A60_CASE_TYPE_VALIDATE_ENDPOINT;
+import static uk.gov.hmcts.reform.bulkscan.utils.Constants.C51_CASE_TYPE_VALIDATE_ENDPOINT;
+import static uk.gov.hmcts.reform.bulkscan.utils.Constants.C63_CASE_TYPE_VALIDATE_ENDPOINT;
 import static uk.gov.hmcts.reform.bulkscan.utils.Constants.CASE_TYPE_TRANSFORM_ENDPOINT;
 import static uk.gov.hmcts.reform.bulkscan.utils.Constants.EdgeCase_CASE_TYPE_VALIDATE_ENDPOINT;
 import static uk.gov.hmcts.reform.bulkscan.utils.Constants.FL401_CASE_TYPE_VALIDATE_ENDPOINT;
@@ -45,6 +48,12 @@ class BulkScanEndpointIntegrationTest {
             "classpath:request/bulk-scan-fl403-transform-input.json";
     private static final String FL403_VALIDATION_RESPONSE_PATH =
             "classpath:response/bulk-scan-fl403-validation-response.json";
+    private static final String C51_VALIDATION_REQUEST_PATH =
+            "classpath:request/bulk-scan-c51-validation-input.json";
+    private static final String C63_VALIDATION_REQUEST_PATH =
+            "classpath:request/bulk-scan-c63-validation-input.json";
+    private static final String A60_VALIDATION_REQUEST_PATH =
+            "classpath:request/bulk-scan-a60-validation-input.json";
 
     @DisplayName("should test validate request case type FL401")
     @Test
@@ -67,6 +76,35 @@ class BulkScanEndpointIntegrationTest {
                 .andExpect(content().json(readFileFrom(FL403_VALIDATION_RESPONSE_PATH)));
     }
 
+    @DisplayName("should test validate request case type C51")
+    @Test
+    void shouldTestValidationRequestCaseTypeC51() throws Exception {
+        mockMvc.perform(post(C51_CASE_TYPE_VALIDATE_ENDPOINT)
+                        .contentType(APPLICATION_JSON)
+                        .header(SERVICE_AUTHORIZATION, SERVICE_AUTHORIZATION_VALUE)
+                        .content(readFileFrom(C51_VALIDATION_REQUEST_PATH)))
+                .andExpect(status().isOk()).andReturn();
+    }
+
+    @DisplayName("should test validate request case type C63")
+    @Test
+    void shouldTestValidationRequestCaseTypeC63() throws Exception {
+        mockMvc.perform(post(C63_CASE_TYPE_VALIDATE_ENDPOINT)
+                        .contentType(APPLICATION_JSON)
+                        .header(SERVICE_AUTHORIZATION, SERVICE_AUTHORIZATION_VALUE)
+                        .content(readFileFrom(C63_VALIDATION_REQUEST_PATH)))
+                .andExpect(status().isOk()).andReturn();
+    }
+
+    @DisplayName("should test validate request case type A60")
+    @Test
+    void shouldTestValidationRequestCaseTypeA60() throws Exception {
+        mockMvc.perform(post(A60_CASE_TYPE_VALIDATE_ENDPOINT)
+                        .contentType(APPLICATION_JSON)
+                        .header(SERVICE_AUTHORIZATION, SERVICE_AUTHORIZATION_VALUE)
+                        .content(readFileFrom(A60_VALIDATION_REQUEST_PATH)))
+                .andExpect(status().isOk()).andReturn();
+    }
 
     @DisplayName("should test validate request case type EdgeCase")
     @Test
