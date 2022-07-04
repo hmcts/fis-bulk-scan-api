96e538cdee803a0ccab0f9f44e31e4850b1ded53
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-07-04 18:02:01 +0100

    ISDB-266: Validation integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/controllers/BulkScanEndpointIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscan/controllers/BulkScanEndpointIntegrationTest.java
@@ -57,6 +57,10 @@ class BulkScanEndpointIntegrationTest {
             "classpath:request/bulk-scan-a58-step-parent-transform-input.json";
     private static final String A58_STEP_PARENT_TRANSFORM_RESPONSE_PATH =
             "classpath:response/bulk-scan-a58-step-parent-transform-output.json";
+    private static final String A59_TRANSFORM_REQUEST_PATH =
+        "classpath:request/bulk-scan-a59-transform-input.json";
+    private static final String A59_TRANSFORM_RESPONSE_PATH =
+        "classpath:response/bulk-scan-a59-transform-output.json";
     private static final String FL403_VALIDATION_RESPONSE_PATH =
             "classpath:response/bulk-scan-fl403-validation-response.json";
     private static final String C51_VALIDATION_REQUEST_PATH =
@@ -182,6 +186,17 @@ class BulkScanEndpointIntegrationTest {
                         .content(readFileFrom(A58_STEP_PARENT_TRANSFORM_REQUEST_PATH)))
                 .andExpect(status().isOk())
                 .andExpect(content().json(readFileFrom(A58_STEP_PARENT_TRANSFORM_RESPONSE_PATH)));
+    }
+
+    @DisplayName("should test transform request case type A59")
+    @Test
+    void shouldTestTransformRequestCaseTypeA59() throws Exception {
+        mockMvc.perform(post(CASE_TYPE_TRANSFORM_ENDPOINT)
+                            .contentType(APPLICATION_JSON)
+                            .header(SERVICE_AUTHORIZATION, SERVICE_AUTHORIZATION_VALUE)
+                            .content(readFileFrom(A59_TRANSFORM_REQUEST_PATH)))
+            .andExpect(status().isOk())
+            .andExpect(content().json(readFileFrom(A59_TRANSFORM_RESPONSE_PATH)));
     }
 
     @DisplayName("should test transform request case type C2")
