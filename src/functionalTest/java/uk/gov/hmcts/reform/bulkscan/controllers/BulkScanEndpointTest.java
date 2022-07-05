10b432bcd3007cdf8f1d828923a7ca4c740f2391
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-07-05 10:58:22 +0100

    ISDB-266: Validation functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/controllers/BulkScanEndpointTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscan/controllers/BulkScanEndpointTest.java
@@ -36,6 +36,14 @@ public class BulkScanEndpointTest {
             "classpath:responses/bulk-scan-a58-step-parent-validation-output.json";
     private static final String A58_STEP_PARENT_TRANSFORM_OUTPUT_PATH =
             "classpath:responses/bulk-scan-a58-step-parent-transform-output.json";
+    private static final String A59_VALIDATION_INPUT_PATH =
+        "classpath:requests/bulk-scan-a59-validation-input.json";
+    private static final String A59_VALIDATION_OUTPUT_PATH =
+        "classpath:responses/bulk-scan-a59-validation-output.json";
+    private static final String A59_TRANSFORM_INPUT_PATH =
+        "classpath:requests/bulk-scan-a59-transform-input.json";
+    private static final String A59_TRANSFORM_OUTPUT_PATH =
+        "classpath:responses/bulk-scan-a59-transform-output.json";
 
     private final String targetInstance =
         StringUtils.defaultIfBlank(
@@ -70,6 +78,25 @@ public class BulkScanEndpointTest {
     }
 
     @Test
+    public void shouldValidate59BulkScanRequest() throws Exception {
+        String bulkScanValidationRequest =
+            readFileFrom(A59_VALIDATION_INPUT_PATH);
+
+        String bulkScanValidationResponse =
+            readFileFrom(A59_VALIDATION_OUTPUT_PATH);
+
+        Response response = request.header(AUTH_HEADER, AUTH_HEADER)
+            .body(bulkScanValidationRequest)
+            .when()
+            .contentType("application/json")
+            .post("forms/A59/validate-ocr");
+
+        response.then().assertThat().statusCode(HttpStatus.OK.value());
+
+        JSONAssert.assertEquals(bulkScanValidationResponse, response.getBody().asString(), true);
+    }
+
+    @Test
     public void shouldTransformA58StepParentBulkScanRequest() throws Exception {
         String bulkScanTransformRequest =
                 readFileFrom(A58_STEP_PARENT_TRANSFORM_INPUT_PATH);
@@ -82,6 +109,25 @@ public class BulkScanEndpointTest {
                 .when()
                 .contentType("application/json")
                 .post("/transform-exception-record");
+
+        response.then().assertThat().statusCode(HttpStatus.OK.value());
+
+        JSONAssert.assertEquals(bulkScanTransformResponse, response.getBody().asString(), true);
+    }
+
+    @Test
+    public void shouldTransformA59BulkScanRequest() throws Exception {
+        String bulkScanTransformRequest =
+            readFileFrom(A59_TRANSFORM_INPUT_PATH);
+
+        String bulkScanTransformResponse =
+            readFileFrom(A59_TRANSFORM_OUTPUT_PATH);
+
+        Response response = request.header(AUTH_HEADER, AUTH_HEADER)
+            .body(bulkScanTransformRequest)
+            .when()
+            .contentType("application/json")
+            .post("/transform-exception-record");
 
         response.then().assertThat().statusCode(HttpStatus.OK.value());
 
