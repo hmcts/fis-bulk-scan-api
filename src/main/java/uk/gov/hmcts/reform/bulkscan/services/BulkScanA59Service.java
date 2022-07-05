b26cf27780164b4815761998e43f5f17e8a3cc5c
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-07-05 15:02:44 +0100

    ISDB-266: Adjusted according to the changes of transformation framework

--- src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanA59Service.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanA59Service.java
@@ -14,14 +14,15 @@ import uk.gov.hmcts.reform.bulkscan.model.CaseCreationDetails;
 import uk.gov.hmcts.reform.bulkscan.model.FormType;
 import uk.gov.hmcts.reform.bulkscan.model.OcrDataField;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.BULK_SCAN_CASE_REFERENCE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.CASE_TYPE_ID;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.EVENT_ID;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.UNKNOWN_FIELDS_MESSAGE;
 
 
 @Service
@@ -58,8 +59,16 @@ public class BulkScanA59Service implements BulkScanService {
 
         caseData.put(BULK_SCAN_CASE_REFERENCE, bulkScanTransformationRequest.getId());
 
-        Map<String, String> inputFieldsMap = inputFieldsList.stream().collect(
-            Collectors.toMap(OcrDataField::getName, OcrDataField::getValue));
+        Map<String, String> inputFieldsMap = getOcrDataFieldAsMap(inputFieldsList);
+
+        BulkScanFormValidationConfigManager
+            .ValidationConfig validationConfig = configManager
+            .getValidationConfig(FormType.valueOf(getCaseType().name()));
+
+        List<String> unknownFieldsList = bulkScanValidationHelper
+            .findUnknownFields(inputFieldsList,
+                               validationConfig.getMandatoryFields(),
+                               validationConfig.getOptionalFields());
 
         Map<String, Object> populatedMap = (Map<String, Object>) BulkScanTransformHelper
             .transformToCaseData(transformConfigManager.getTransformationConfig(FormType.A59)
@@ -68,10 +77,20 @@ public class BulkScanA59Service implements BulkScanService {
         Map<String, String> caseTypeAndEventId =
             transformConfigManager.getTransformationConfig(FormType.valueOf(getCaseType().name())).getCaseFields();
 
-        return BulkScanTransformationResponse.builder().caseCreationDetails(
-            CaseCreationDetails.builder()
-                .caseTypeId(caseTypeAndEventId.get(CASE_TYPE_ID))
-                .eventId(caseTypeAndEventId.get(EVENT_ID))
-                .caseData(populatedMap).build()).build();
+        BulkScanTransformationResponse.BulkScanTransformationResponseBuilder builder = BulkScanTransformationResponse
+            .builder().caseCreationDetails(
+                CaseCreationDetails.builder()
+                    .caseTypeId(caseTypeAndEventId.get(CASE_TYPE_ID))
+                    .eventId(caseTypeAndEventId.get(EVENT_ID))
+                    .caseData(populatedMap).build());
+
+        if (null != unknownFieldsList && !unknownFieldsList.isEmpty()) {
+            builder.warnings(Arrays.asList(String.format(
+                UNKNOWN_FIELDS_MESSAGE,
+                String.join(",", unknownFieldsList)
+            )));
+        }
+
+        return builder.build();
     }
 }
