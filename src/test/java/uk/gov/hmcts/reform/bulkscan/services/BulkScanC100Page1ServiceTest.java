77241c1a29c408d6bfe07fe46d737fc23c999ad3
RonanCog <ronan.behan@HMCTS.NET>
2022-07-20 15:15:05 +0100

    Bulk scan phase 2-Private Law non Edge Cases-Child Arrangement- Page 1 2nd commit

--- src/test/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanC100Page1ServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanC100Page1ServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.bulkscan.services;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.junit.FixMethodOrder;
