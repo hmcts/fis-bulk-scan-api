cda3c08dbc7ddf04122467a057cc9e3fce131d10
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-08-04 15:43:12 +0100

    changs for nested key transformation

--- src/main/java/uk/gov/hmcts/reform/bulkscan/config/BulkScanTransformConfigManager.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/config/BulkScanTransformConfigManager.java
@@ -1,6 +1,12 @@
 package uk.gov.hmcts.reform.bulkscan.config;
 
+import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Map;
+
+import com.microsoft.applicationinsights.core.dependencies.google.common.reflect.TypeToken;
+import com.microsoft.applicationinsights.core.dependencies.google.gson.Gson;
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,7 +25,17 @@ public class BulkScanTransformConfigManager {
     @Data
     public static class TransformationConfig {
         Map<String, String> caseFields;
+        @Getter(AccessLevel.NONE)
         Map<String, Object> caseDataFields;
+
+        /***
+         * This mehtod will return the deep copy of hashmap instead of touching the original one.
+         * */
+        public Map<String, Object> getCaseDataFields() {
+            Gson gson = new Gson();
+            return gson.fromJson(gson.toJson(this.caseDataFields),
+                                                              new TypeToken<HashMap<String, Object>>(){}.getType());
+        }
     }
 
     public TransformationConfig getTransformationConfig(FormType formType) {
