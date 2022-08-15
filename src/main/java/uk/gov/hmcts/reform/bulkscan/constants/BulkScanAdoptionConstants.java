5c6366b2678e1bf341392bf7f3773716f990f02a
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-08-15 12:45:23 +0100

    ISDB-344: Refactoring

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/constants/BulkScanAdoptionConstants.java
@@ -0,0 +1,141 @@
+package uk.gov.hmcts.reform.bulkscan.constants;
+
+public final class BulkScanAdoptionConstants {
+    // A58
+    public static final String CHILD_PLACEMENT_ORDER_BY_ENGLAND_AND_WALES_COURT =
+            "child_placementOrderByEnglandAndWalesCourt";
+    public static final String CHILD_PLACMENT_ORDER_BY_ENGLAND_AND_WALES_COURT_NAME =
+            "child_placmentOrderByEnglandAndWalesCourtName";
+    public static final String CHILD_PLACMENT_ORDER_BY_ENGLAND_AND_WALES_CASE_NUMBER =
+            "child_placmentOrderByEnglandAndWalesCaseNumber";
+    public static final String CHILD_PLACMENT_ORDER_BY_ENGLAND_AND_WALES_TYPE_OF_ORDER =
+            "child_placmentOrderByEnglandAndWalesTypeOfOrder";
+    public static final String CHILD_PLACMENT_ORDER_BY_ENGLAND_AND_WALES_DATE_OF_ORDER =
+            "child_placmentOrderByEnglandAndWalesDateOfOrder";
+    public static final String CHILD_FREEING_ORDER_BY_ENGLAND_AND_WALES_COURT =
+            "child_freeingOrderByEnglandAndWalesCourt";
+    public static final String CHILD_FREEING_ORDER_BY_ENGLAND_AND_WALES_COURT_NAME =
+            "child_freeingOrderByEnglandAndWalesCourtName";
+    public static final String CHILD_FREEING_ORDER_BY_ENGLAND_AND_WALES_CASE_NUMBER =
+            "child_freeingOrderByEnglandAndWalesCaseNumber";
+    public static final String CHILD_FREEING_ORDER_BY_ENGLAND_AND_WALES_TYPE_OF_ORDER =
+            "child_freeingOrderByEnglandAndWalesTypeOfOrder";
+    public static final String CHILD_FREEING_ORDER_BY_ENGLAND_AND_WALES_DATE_OF_ORDER =
+            "child_freeingOrderByEnglandAndWalesDateOfOrder";
+    public static final String CHILD_FREEING_ORDER_BY_NORTHERN_IRELAND_COURT =
+            "child_freeingOrderByNorthernIrelandCourt";
+    public static final String CHILD_FREEING_ORDER_BY_NORTHERN_IRELAND_COURT_NAME =
+            "child_freeingOrderByNorthernIrelandCourtName";
+    public static final String CHILD_FREEING_ORDER_BY_NORTHERN_IRELAND_CASE_NUMBER =
+            "child_freeingOrderByNorthernIrelandCaseNumber";
+    public static final String CHILD_FREEING_ORDER_BY_NORTHERN_IRELAND_TYPE_OF_ORDER =
+            "child_freeingOrderByNorthernIrelandTypeOfOrder";
+    public static final String CHILD_FREEING_ORDER_BY_NORTHERN_IRELAND_DATE_OF_ORDER =
+            "child_freeingOrderByNorthernIrelandDateOfOrder";
+    public static final String CHILD_PERMANENCE_ORDER_BY_SCOTLAND_COURT =
+            "child_permanenceOrderByScotlandCourt";
+    public static final String CHILD_PERMANENCE_ORDER_BY_SCOTLAND_COURT_NAME =
+            "child_permanenceOrderByScotlandCourtName";
+    public static final String CHILD_PERMANENCE_ORDER_BY_SCOTLAND_CASE_NUMBER =
+            "child_permanenceOrderByScotlandCaseNumber";
+    public static final String CHILD_PERMANENCE_ORDER_BY_SCOTLAND_TYPE_OF_ORDER =
+            "child_permanenceOrderByScotlandTypeOfOrder";
+    public static final String CHILD_PERMANENCE_ORDER_BY_SCOTLAND_DATE_OF_ORDER =
+            "child_permanenceOrderByScotlandDateOfOrder";
+    public static final String CHILD_NO_ORDER_AVAILABLE = "child_noOrderAvailable";
+    public static final String ANY_OTHER_ORDERS_AVAILABLE = "anyOtherOrdersAvailable";
+    public static final String PLACEMENT_ORDER_COURT = "placementOrderCourt";
+    public static final String PLACEMENT_ORDER_ID = "placementOrderId";
+    public static final String PLACEMENT_ORDER_TYPE = "placementOrderType";
+    public static final String PLACEMENT_ORDER_DATE = "placementOrderDate";
+    public static final String FREEING_ORDER_COURT = "freeingOrderCourt";
+    public static final String FREEING_ORDER_ID = "freeingOrderId";
+    public static final String FREEING_ORDER_TYPE = "freeingOrderType";
+    public static final String FREEING_ORDER_DATE = "freeingOrderDate";
+    public static final String CHILD_NO_LA_OR_PARENTAL_RESPONSIBILITY =
+            "child_noLaOrParentalResponsibility";
+    public static final String LA_OR_PARENTAL_RESPONSIBILITY = "laOrParentalResponsibility";
+    public static final String MAINTANENCE_ORDER = "maintanenceOrder";
+    public static final String CHILD_COURT_AND_DATE_OF_ORDER = "child_courtAndDateOfOrder";
+    public static final String NAME_OF_COURT = "nameOfCourt";
+    public static final String DATE_OF_ORDER = "dateOfOrder";
+    public static final String CHILD_NO_MAINTANENCE_ORDER = "child_noMaintanenceOrder";
+    public static final String CHILD_MAINTANENCE_ORDER = "child_maintanenceOrder";
+    public static final String HAS_MAINTANENCE_ORDER = "hasMaintanenceOrder";
+    public static final String CHILD_NO_PROCEEDING_DETAILS = "child_noProceedingDetails";
+    public static final String HAS_PROCEEDING_DETAILS = "hasProceedingDetails";
+    public static final String CHILD_PROCEEDING_DETAILS = "child_proceedingDetails";
+    public static final String CHILD_NO_PROCEEDING_DETAILS_WITH_RELATION =
+            "child_noProceedingDetailsWithRelation";
+    public static final String HAS_PROCEEDING_DETAILS_WITH_RELATION =
+            "hasProceedingDetailsWithRelation";
+    public static final String CHILD_PROCEEDING_DETAILS_WITH_RELATION =
+            "child_proceedingDetailsWithRelation";
+    public static final String CHILD_DONT_KNOW_PROCEEDING_DETAILS_WITH_RELATION =
+            "child_dontKnowProceedingDetailsWithRelation";
+    public static final String DONT_KNOW = "DontKnow";
+    public static final String TYPE_OF_ORDER = "typeOfOrder";
+    public static final String CASE_NUMBER = "caseNumber";
+    public static final String CASE_TYPE_CATEGORY = "caseTypeCategory ";
+    public static final String CHILD_TYPE_OF_ORDER = "child_typeOfOrder";
+    public static final String CHILD_DATE_OF_ORDER = "child_dateOfOrder";
+    public static final String CHILD_NAME_OF_COURT = "child_nameOfCourt";
+    public static final String CHILD_CASE_NUMBER = "child_caseNumber";
+    public static final String ABOUT_OTHER_ORDERS_OR_PROCEEDINGS_THAT_AFFECT_THE_CHILD =
+            "About other orders or proceedings that affect the child";
+    public static final String CHILD_TYPE_OF_ORDER_RELATION = "child_typeOfOrderRelation";
+    public static final String CHILD_DATE_OF_ORDER_RELATION = "child_dateOfOrderRelation";
+    public static final String CHILD_NAME_OF_COURT_RELATION = "child_nameOfCourtRelation";
+    public static final String CHILD_CASE_NUMBER_RELATION = "child_caseNumberRelation";
+    public static final String CASES_CONCERNING_A_RELATED_CHILD =
+            "Cases concerning a related child";
+    public static final String ADOP_AGENCY_OR_L_AS = "adopAgencyOrLAs";
+    public static final String CHILD_PLACED_ADOPTION_DATE = "child_placedAdoptionDate";
+    public static final String ADOP_AGENCY_OR_LA_NAME = "adopAgencyOrLaName";
+    public static final String ADOP_AGENCY_OR_LAADDRESS = "adopAgencyOrLaaddress";
+    public static final String ADOP_AGENCY_OR_LA_CONTACT_NAME = "adopAgencyOrLaContactName";
+    public static final String ADOP_AGENCY_OR_LA_PHONE_NUMBER = "adopAgencyOrLaPhoneNumber";
+    public static final String L_AOR_ADOPTION_AGENCY_CATEGORY = "LAorAdoptionAgencyCategory";
+    public static final String CHILD_PLACED_ADOPTION_AGENCY_NAME = "child_placedAdoptionAgencyName";
+    public static final String CHILD_PLACED_ADOPTION_AGENCY_ADDRESS =
+            "child_placedAdoptionAgencyAddress";
+    public static final String CHILD_PLACED_ADOPTION_AGENCY_CONTACT_NAME =
+            "child_placedAdoptionAgencyContactName";
+    public static final String CHILD_PLACED_ADOPTION_AGENCY_TELEPHONE_NO =
+            "child_placedAdoptionAgencyTelephoneNo";
+    public static final String CHILD_PLACED_FOR_THE_PURPOSE_OF_ADOPTION =
+            "Child placed for the purpose of Adoption";
+    public static final String CHILD_IS_ADOPTION_AGENCY_INVOLVED = "child_isAdoptionAgencyInvolved";
+    public static final String CHILD_IS_ADOPTION_AGENCY_DETAILS_AVAILABLE =
+            "child_isAdoptionAgencyDetailsAvailable";
+    public static final String CHILD_ADOPTION_AGENCY_NAME = "child_adoptionAgencyName";
+    public static final String CHILD_ADOPTION_AGENCY_ADDRESS = "child_adoptionAgencyAddress";
+    public static final String CHILD_ADOPTION_AGENCY_CONTACT_NAME =
+            "child_adoptionAgencyContactName";
+    public static final String CHILD_ADOPTION_AGENCY_TELEPHONE_NO =
+            "child_adoptionAgencyTelephoneNo";
+    public static final String INVOLVED_IN_THE_PLACING_OF_THE_CHILD =
+            "Involved in the placing of the child";
+    public static final String CHILD_LA_NAME = "child_laName";
+    public static final String CHILD_LA_ADDRESS = "child_laAddress";
+    public static final String CHILD_LA_CONTACT_NAME = "child_laContactName";
+    public static final String CHILD_LA_TELEPHONE_NO = "child_laTelephoneNo";
+    public static final String NOTIFY_LOCAL_AUTHORITY_INTENTION_OF_ADOPTION =
+            "Notify Local Authority intention of Adoption";
+    public static final String CHILD_LA_OR_PARENTAL_RESPONSIBILITY_DETAILS =
+            "child_LaOrParentalResponsibilityDetails";
+    public static final String CHILD_LA_OR_PARENTAL_NAME = "child_laOrParentalName";
+    public static final String CHILD_LA_OR_PARENTAL_ADDRESS = "child_laOrParentalAddress";
+    public static final String CHILD_LA_OR_PARENTAL_CONTACT_NAME = "child_laOrParentalContactName";
+    public static final String CHILD_LA_OR_PARENTAL_TELEPHONE_NO = "child_laOrParentalTelephoneNo";
+    public static final String CHILD_LA_OR_PARENTAL_EMAIL = "child_laOrParentalEmail";
+    public static final String LOCAL_AUTHORITY_HAS_PARENTAL_RESPONSIBILITY_OF_THE_CHILD =
+            "Local Authority has parental responsibility of the Child";
+    public static final String ADOP_AGENCY_OR_LA_CONTACT_EMAIL = "adopAgencyOrLaContactEmail ";
+    public static final String ADOPTION_DATE = "adoptionDate";
+    public static final String PROCEEDING_DETAILS = "proceedingDetails";
+    public static final String IS_ADOPTION_AGENCY_INVOLVED = "isAdoptionAgencyInvolved";
+
+    // End of A58
+    private BulkScanAdoptionConstants() {}
+}
