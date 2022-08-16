dc0d8de78f64068eb89114a137f83d0330842fa4
parvezhmcts <parvez.qureshi@hmcts.net>
2022-08-16 08:58:43 +0100

    ISDB-296
    - C100 form section 4 changes.
    - Fixed test coverage

--- src/test/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanC100ServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanC100ServiceTest.java
@@ -81,6 +81,12 @@ class BulkScanC100ServiceTest {
     private static final String C100_TRANSFORM_RESPONSE_PATH =
             "classpath:response/bulk-scan-c100-transform-output.json";
 
+    private static final String C100_TRANSFORM_SECTION4_SCENARIO1_REQUEST_PATH =
+            "classpath:request/bulk-scan-c100-section4-secenario-1-transform-input.json";
+
+    private static final String C100_TRANSFORM_SECTION4_SCENARIO1_RESPONSE_PATH =
+            "classpath:response/bulk-scan-c100-section4-scenario1-transform-output.json";
+
     @Autowired BulkScanC100Service bulkScanValidationService;
 
     @MockBean PostcodeLookupService postcodeLookupService;
@@ -830,5 +836,21 @@ class BulkScanC100ServiceTest {
                                         .get(OTHER_PROCEEDINGS_DETAILS_TABLE))
                         .size(),
                 1);
+    }
+
+    @Test
+    @DisplayName("C100 transform success section 4 - scenario 1.")
+    void testC100TransformSuccessSection4Scenario1() throws IOException, JSONException {
+        BulkScanTransformationRequest bulkScanTransformationRequest =
+                mapper.readValue(
+                        readFileFrom(C100_TRANSFORM_SECTION4_SCENARIO1_REQUEST_PATH),
+                        BulkScanTransformationRequest.class);
+
+        BulkScanTransformationResponse res =
+                bulkScanValidationService.transform(bulkScanTransformationRequest);
+        JSONAssert.assertEquals(
+                readFileFrom(C100_TRANSFORM_SECTION4_SCENARIO1_RESPONSE_PATH),
+                mapper.writeValueAsString(res),
+                true);
     }
 }
