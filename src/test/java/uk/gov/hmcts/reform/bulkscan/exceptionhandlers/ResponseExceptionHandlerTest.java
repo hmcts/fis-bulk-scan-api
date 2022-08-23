b00dba47f7d9deb15f6fed84ea388fffffbc7fac
RonanCog <ronan.behan@HMCTS.NET>
2022-08-23 15:19:44 +0100

    Adding token (3)

--- src/test/java/uk/gov/hmcts/reform/bulkscan/exceptionhandlers/ResponseExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/exceptionhandlers/ResponseExceptionHandlerTest.java
@@ -1,13 +1,13 @@
 package uk.gov.hmcts.reform.bulkscan.exceptionhandlers;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import com.sun.jdi.InternalException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
 import uk.gov.hmcts.reform.bulkscan.exception.ForbiddenException;
 import uk.gov.hmcts.reform.bulkscan.exception.UnauthorizedException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ResponseExceptionHandlerTest {
 
@@ -20,21 +20,29 @@ class ResponseExceptionHandlerTest {
 
     @Test
     void handleInvalidTokenException() {
-        assertNotNull(responseExceptionHandler.handleInvalidTokenException(new InvalidTokenException("Test Exception")));
+        assertNotNull(
+                responseExceptionHandler.handleInvalidTokenException(
+                        new InvalidTokenException("Test Exception")));
     }
 
     @Test
     void handleUnAuthorizedException() {
-        assertNotNull(responseExceptionHandler.handleUnAuthorizedException(new UnauthorizedException("Test Exception")));
+        assertNotNull(
+                responseExceptionHandler.handleUnAuthorizedException(
+                        new UnauthorizedException("Test Exception")));
     }
 
     @Test
     void handleForbiddenException() {
-        assertNotNull(responseExceptionHandler.handleForbiddenException(new ForbiddenException("Test Exception")));
+        assertNotNull(
+                responseExceptionHandler.handleForbiddenException(
+                        new ForbiddenException("Test Exception")));
     }
 
     @Test
     void handleInternalException() {
-        assertNotNull(responseExceptionHandler.handleInternalException(new InternalException("Test Exception")));
+        assertNotNull(
+                responseExceptionHandler.handleInternalException(
+                        new InternalException("Test Exception")));
     }
 }
