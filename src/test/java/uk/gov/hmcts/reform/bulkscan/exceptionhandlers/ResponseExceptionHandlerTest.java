27c2d7ab694d6372d5f1f03b0aaf79feeeb7611a
RonanCog <ronan.behan@HMCTS.NET>
2022-08-23 15:13:53 +0100

    Adding token (2)

---
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/exceptionhandlers/ResponseExceptionHandlerTest.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.bulkscan.exceptionhandlers;
+
+import com.sun.jdi.InternalException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
+import uk.gov.hmcts.reform.bulkscan.exception.ForbiddenException;
+import uk.gov.hmcts.reform.bulkscan.exception.UnauthorizedException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResponseExceptionHandlerTest {
+
+    ResponseExceptionHandler responseExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        responseExceptionHandler = new ResponseExceptionHandler();
+    }
+
+    @Test
+    void handleInvalidTokenException() {
+        assertNotNull(responseExceptionHandler.handleInvalidTokenException(new InvalidTokenException("Test Exception")));
+    }
+
+    @Test
+    void handleUnAuthorizedException() {
+        assertNotNull(responseExceptionHandler.handleUnAuthorizedException(new UnauthorizedException("Test Exception")));
+    }
+
+    @Test
+    void handleForbiddenException() {
+        assertNotNull(responseExceptionHandler.handleForbiddenException(new ForbiddenException("Test Exception")));
+    }
+
+    @Test
+    void handleInternalException() {
+        assertNotNull(responseExceptionHandler.handleInternalException(new InternalException("Test Exception")));
+    }
+}
