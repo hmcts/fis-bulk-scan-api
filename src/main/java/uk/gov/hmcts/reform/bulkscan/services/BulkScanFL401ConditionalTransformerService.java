5a3341fd28b74ac26dc103da017f518734a56d9e
parvezhmcts <parvez.qureshi@hmcts.net>
2022-09-14 13:16:32 +0100

    ISDB-361
    - FL401 section 2 transformation changes

--- src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanFL401ConditionalTransformerService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanFL401ConditionalTransformerService.java
@@ -1,9 +1,23 @@
 package uk.gov.hmcts.reform.bulkscan.services;
 
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.NO;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.YES;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.ADDRESS;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.ADDRESS_LINE1;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.ADDRESS_LINE2;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_ADDRESS_BUILDING_AND_STREET;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_ADDRESS_COUNTY;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_ADDRESS_POSTCODE;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_ADDRESS_SECOND_LINE;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_ADDRESS_TOWN_OR_CITY;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_CONTACT_CONFIDENTIALITY;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_EMAIL;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_PHONE_NUMBER;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_PREFER_TO_BE_CONTACTED;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_RESPONDENT_OTHER_RELATIONSHIP_FIELD;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_RESPONDENT_RELATIONSHIP_FIELDS;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_RESPONDENT_RELATIONSHIP_OPTIONS_FIELDS;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.APPLICANT_SOLICITOR_NAME;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.ATTEND_HEARING_TABLE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.BAIL_CONDITION_END_DATE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.CCD_APPLICANT_RELATIONSHIOP_DATE;
@@ -11,11 +25,25 @@ import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.CCD_
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.CCD_RELATIONSHIP_DATE_COMPLEX_START_DATE_FIELD;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.CCD_RELATIONSHIP_TO_RESPONDENT_TABLE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.COMMA;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.COUNTY;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.DELIVERATELY_EVADING_SERVICE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.DETERREDOR_PREVENTED;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.EMAIL;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.EMPTY_SPACE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.FL401_APPLICANT_RELATIONSHIP;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.FL401_APPLICANT_RELATIONSHIP_OPTIONS;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.FL401_APPLICANT_TABLE;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.FL401_SOLICITOR_TABLE;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.IS_ADDRESS_CONFIDENTIAL;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.IS_EMAIL_ADDRESS_CONFIDENTIAL;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.IS_PHONE_NUMBER_CONFIDENTIAL;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.PHONE_NUMBER;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.POSTCODE;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.POSTTOWN;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.PREFER_TO_BE_CONTACTED;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.REASON_FOR_ORDER_WITHOUT_GIVING_NOTICE;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.REPRESENTATIVE_FIRST_NAME;
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.REPRESENTATIVE_LAST_NAME;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.RISK_OF_SIGNIFICANT_HARM;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.SPACE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.SPECIAL_MEASURE_AT_COURT;
@@ -132,6 +160,22 @@ public class BulkScanFL401ConditionalTransformerService {
 
         attendHearingTableMap.put(
                 SPECIAL_MEASURE_AT_COURT, getFormattedSpecialMeasureAtCourt(inputFieldsMap));
+
+        LinkedTreeMap fl401ApplicantTable = (LinkedTreeMap) populatedMap.get(FL401_APPLICANT_TABLE);
+
+        String applicantContactConfidentiality =
+                inputFieldsMap.get(APPLICANT_CONTACT_CONFIDENTIALITY);
+
+        transformApplicantConfidentiality(fl401ApplicantTable, applicantContactConfidentiality);
+        fl401ApplicantTable.put(
+                ADDRESS,
+                transformApplicantAddress(inputFieldsMap, applicantContactConfidentiality));
+        transformContactDetails(
+                inputFieldsMap, fl401ApplicantTable, applicantContactConfidentiality);
+
+        LinkedTreeMap fl401SolicitorDetailsTable =
+                (LinkedTreeMap) populatedMap.get(FL401_SOLICITOR_TABLE);
+        transformSolicitorName(inputFieldsMap, fl401SolicitorDetailsTable);
     }
 
     private String getFormattedSpecialMeasureAtCourt(Map<String, String> inputFieldsMap) {
@@ -214,5 +258,66 @@ public class BulkScanFL401ConditionalTransformerService {
         }
 
         return null;
+    }
+
+    private void transformApplicantConfidentiality(
+            LinkedTreeMap fl401ApplicantTable, String applicantContactConfidentiality) {
+
+        if (null != applicantContactConfidentiality
+                && applicantContactConfidentiality.equalsIgnoreCase(YES)) {
+
+            fl401ApplicantTable.put(IS_ADDRESS_CONFIDENTIAL, YES);
+            fl401ApplicantTable.put(IS_PHONE_NUMBER_CONFIDENTIAL, YES);
+            fl401ApplicantTable.put(IS_EMAIL_ADDRESS_CONFIDENTIAL, YES);
+
+        } else if (null != applicantContactConfidentiality
+                && applicantContactConfidentiality.equalsIgnoreCase(NO)) {
+            fl401ApplicantTable.put(IS_ADDRESS_CONFIDENTIAL, NO);
+            fl401ApplicantTable.put(IS_PHONE_NUMBER_CONFIDENTIAL, NO);
+            fl401ApplicantTable.put(IS_EMAIL_ADDRESS_CONFIDENTIAL, NO);
+        }
+    }
+
+    private LinkedTreeMap transformApplicantAddress(
+            Map<String, String> inputFieldsMap, String applicantContactConfidentiality) {
+        LinkedTreeMap address = new LinkedTreeMap<>();
+        if (null != applicantContactConfidentiality
+                && applicantContactConfidentiality.equalsIgnoreCase(NO)) {
+            address.put(ADDRESS_LINE1, inputFieldsMap.get(APPLICANT_ADDRESS_BUILDING_AND_STREET));
+            address.put(ADDRESS_LINE2, inputFieldsMap.get(APPLICANT_ADDRESS_SECOND_LINE));
+            address.put(POSTTOWN, inputFieldsMap.get(APPLICANT_ADDRESS_TOWN_OR_CITY));
+            address.put(COUNTY, inputFieldsMap.get(APPLICANT_ADDRESS_COUNTY));
+            address.put(POSTCODE, inputFieldsMap.get(APPLICANT_ADDRESS_POSTCODE));
+        }
+        return address;
+    }
+
+    private void transformContactDetails(
+            Map<String, String> inputFieldsMap,
+            LinkedTreeMap fl401ApplicantTable,
+            String applicantContactConfidentiality) {
+        if (null != applicantContactConfidentiality
+                && applicantContactConfidentiality.equalsIgnoreCase(NO)) {
+
+            fl401ApplicantTable.put(PHONE_NUMBER, inputFieldsMap.get(APPLICANT_PHONE_NUMBER));
+            fl401ApplicantTable.put(EMAIL, inputFieldsMap.get(APPLICANT_EMAIL));
+            ;
+            fl401ApplicantTable.put(
+                    PREFER_TO_BE_CONTACTED, inputFieldsMap.get(APPLICANT_PREFER_TO_BE_CONTACTED));
+        }
+    }
+
+    private void transformSolicitorName(
+            Map<String, String> inputFieldsMap, LinkedTreeMap fl401SolicitorDetailsTable) {
+
+        final String applicantSolicitorName = inputFieldsMap.get(APPLICANT_SOLICITOR_NAME);
+
+        if (applicantSolicitorName != null) {
+            final String[] representativeName = applicantSolicitorName.split(EMPTY_SPACE);
+            fl401SolicitorDetailsTable.put(REPRESENTATIVE_FIRST_NAME, representativeName[0]);
+            if (representativeName.length == 2) {
+                fl401SolicitorDetailsTable.put(REPRESENTATIVE_LAST_NAME, representativeName[1]);
+            }
+        }
     }
 }
