a29b4e67fb31a5e6be3fd60f8aff3de1875b77fd
RonanCog <ronan.behan@HMCTS.NET>
2022-09-14 14:37:17 +0100

    feature/ISDB-367 being done on this branch as they are transform and validate of the same section 7

--- src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanFL401ConditionalTransformerService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanFL401ConditionalTransformerService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.bulkscan.services;
 
+import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.VALUE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanConstants.YES;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.BAIL_CONDITION_END_DATE;
 import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.COMMA;
@@ -13,7 +14,11 @@ import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.WITH
 import static uk.gov.hmcts.reform.bulkscan.enums.OrderWithouGivingNoticeReasonEnum.RISKOF_SIGNIFICANT_HARM;
 
 import com.microsoft.applicationinsights.core.dependencies.google.gson.internal.LinkedTreeMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bulkscan.enums.OrderWithouGivingNoticeReasonEnum;
 import uk.gov.hmcts.reform.bulkscan.utils.DateUtil;
@@ -39,6 +44,66 @@ public class BulkScanFL401ConditionalTransformerService {
         withoutNoticeOrderTableMap.put(
                 REASON_FOR_ORDER_WITHOUT_GIVING_NOTICE,
                 transformReasonForOrderWithoutGivingNotice(inputFieldsMap));
+
+        populatedMap.put("children", buildTransformChild(populatedMap, inputFieldsMap));
+    }
+
+    private List buildTransformChild(Map<String, Object> populatedMap, Map<String, String> inputFieldsMap) {
+
+        ArrayList<LinkedTreeMap> children = new ArrayList<>();
+
+        String row1 = inputFieldsMap.get("childLivesAtAddress_Row1");
+        String row2 = inputFieldsMap.get("childLivesAtAddress_Row2");
+        String row3 = inputFieldsMap.get("childLivesAtAddress_Row3");
+        String row4 = inputFieldsMap.get("childLivesAtAddress_Row4");
+
+        String otherChildrenRow1 = inputFieldsMap.get("otherChildren_Row1");
+        String otherChildrenRow2 = inputFieldsMap.get("otherChildren_Row2");
+        String otherChildrenRow3 = inputFieldsMap.get("otherChildren_Row3");
+        String otherChildrenRow4 = inputFieldsMap.get("otherChildren_Row4");
+
+        if(row1!=null){
+            populatedMap.put("doAnyChildrenLiveAtAddress", BooleanUtils.YES);
+        }
+
+        ArrayList<String> childInput = new ArrayList<>();
+        childInput.add(row1);
+        childInput.add(row2);
+        childInput.add(row3);
+        childInput.add(row4);
+        childInput.add(otherChildrenRow1);
+        childInput.add(otherChildrenRow2);
+        childInput.add(otherChildrenRow3);
+        childInput.add(otherChildrenRow4);
+
+        for (String input : childInput) {
+            LinkedTreeMap<String, String> childDetails = new LinkedTreeMap<>();
+
+            final LinkedTreeMap<String, LinkedTreeMap<String, String>> childrenLinkedTreeMap =
+                    new LinkedTreeMap();
+
+            if (null != input) {
+                final String[] columnDetails = input.split(",");
+
+                final String childName = columnDetails[0];
+
+                String childAge = null;
+                String isRespondentResponsibleForChild = null;
+                if (columnDetails.length > 2) {
+                    childAge = columnDetails[1];
+                    isRespondentResponsibleForChild = columnDetails[2];
+                }
+
+                childDetails.put("childFullName", childName);
+                childDetails.put("childsAge", childAge);
+                childDetails.put("isRespondentResponsibleForChild", isRespondentResponsibleForChild);
+
+                childrenLinkedTreeMap.put(VALUE, childDetails);
+
+                children.add(childrenLinkedTreeMap);
+            }
+        }
+        return children;
     }
 
     private String transformReasonForOrderWithoutGivingNotice(Map<String, String> inputFieldsMap) {
