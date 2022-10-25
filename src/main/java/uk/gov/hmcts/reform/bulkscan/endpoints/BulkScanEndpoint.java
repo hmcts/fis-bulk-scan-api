678f417c4fb25757d9c7ce12069f01de77ae4182
hmcts-kodatala <purushotham.kodatala@hmcts.net>
2022-10-25 16:21:06 +0100

    updated logs

--- src/main/java/uk/gov/hmcts/reform/bulkscan/endpoints/BulkScanEndpoint.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/endpoints/BulkScanEndpoint.java
@@ -64,6 +64,7 @@ public class BulkScanEndpoint {
             @RequestHeader(CONTENT_TYPE) String contentType,
             @PathVariable("form-type") String formType,
             @RequestBody final BulkScanValidationRequest bulkScanValidationRequest) {
+        logger.info("Request received to validate ocr data from service {}", bulkScanValidationRequest);
 
         if (formType == null || !EnumUtils.isValidEnum(FormType.class, formType)) {
             logger.error("Invalid form type {} received when validating bulk scan", formType);
@@ -79,7 +80,7 @@ public class BulkScanEndpoint {
         BulkScanValidationResponse bulkScanResponse =
                 Objects.requireNonNull(BulkScanServiceFactory.getService(formTypeEnum))
                         .validate(bulkScanValidationRequest);
-
+        logger.info("response object from service {}", bulkScanResponse);
         return ok(bulkScanResponse);
     }
 
@@ -114,9 +115,10 @@ public class BulkScanEndpoint {
             @RequestHeader(SERVICEAUTHORIZATION) String s2sToken,
             @RequestHeader(CONTENT_TYPE) String contentType,
             @RequestBody final BulkScanTransformationRequest bulkScanTransformationRequest) {
+        logger.info("Request received to transformationOcrData ocr data from service {}", bulkScanTransformationRequest);
 
         String serviceName = authService.authenticate(s2sToken);
-        logger.info("Request received to validate ocr data from service {}", serviceName);
+        logger.info("Request received to transformationOcrData ocr data from service {}", serviceName);
 
         authService.assertIsAllowedToHandleService(serviceName);
 
