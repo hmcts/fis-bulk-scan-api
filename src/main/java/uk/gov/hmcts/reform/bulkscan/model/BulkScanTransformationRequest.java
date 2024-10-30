1f7cb2213c211f7244f9e2b27f041d26391a50b8
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-10-30 14:04:25 +0000

    Ignore unknown properties

--- src/main/java/uk/gov/hmcts/reform/bulkscan/model/BulkScanTransformationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/model/BulkScanTransformationRequest.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.reform.bulkscan.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,53 +13,42 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BulkScanTransformationRequest {
 
-    @NotNull(message = "exception_record_case_type_id cannot be null")
     @JsonProperty("exception_record_case_type_id")
     public String caseTypeId;
 
-    @NotNull(message = "po_box type cannot be null")
     @JsonProperty("po_box")
     public String poBox;
 
-    @NotNull(message = "po_box_jurisdiction type cannot be null")
     @JsonProperty("po_box_jurisdiction")
     public String poBoxJurisdiction;
 
-    @NotNull(message = "form_type type cannot be null")
     @JsonProperty("form_type")
     public String formType;
 
-    @NotNull(message = "journey_classification type cannot be null")
     @JsonProperty("journey_classification")
     public JourneyClassification journeyClassification;
 
-    @NotNull(message = "delivery_date type cannot be null")
     @JsonProperty("delivery_date")
     public LocalDateTime deliveryDate;
 
-    @NotNull(message = "opening_date type cannot be null")
     @JsonProperty("opening_date")
     public LocalDateTime openingDate;
 
-    @NotNull(message = "scanned_documents type cannot be null")
     @JsonProperty("scanned_documents")
     public List<ScannedDocuments> scannedDocuments;
 
-    @NotNull(message = "ocr_data_fields type cannot be null")
     @JsonProperty("ocr_data_fields")
     public List<OcrDataField> ocrdatafields;
 
-    @NotNull(message = "exception_record_id type cannot be null")
     @JsonProperty("exception_record_id")
     public String exceptionRecordId;
 
-    @NotNull(message = "envelope_id type cannot be null")
     @JsonProperty("envelope_id")
     public String envelopeId;
 
-    @NotNull(message = "is_automated_process type cannot be null")
     @JsonProperty("is_automated_process")
     public boolean isAutomatedProcess;
 }
