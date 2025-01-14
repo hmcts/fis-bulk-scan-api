ba14568c7f65a8a3f5e8b16a1faa3337c65f37f5
bharadwajsairam <bharadwajsairam001@gmail.com>
2025-01-14 15:22:55 +0000

    checkstyle test fix

--- src/test/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanC100ServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanC100ServiceTest.java
@@ -53,7 +53,6 @@ import org.json.JSONException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
