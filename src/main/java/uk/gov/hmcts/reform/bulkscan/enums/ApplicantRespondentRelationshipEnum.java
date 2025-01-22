de5e6fc6f259f1d3b0e79f3319754cb132dee768
bharadwajsairam <bharadwajsairam001@gmail.com>
2025-01-22 00:08:05 +0000

    Update enum

--- src/main/java/uk/gov/hmcts/reform/bulkscan/enums/ApplicantRespondentRelationshipEnum.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/enums/ApplicantRespondentRelationshipEnum.java
@@ -4,31 +4,31 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ApplicantRespondentRelationshipEnum {
-    applicantRespondent_Relationship_1("Married or in a civil partnership"),
-    applicantRespondent_Relationship_2("Formerly married or in a civil partnership"),
-    applicantRespondent_Relationship_3("Engaged or proposed civil partnership"),
-    applicantRespondent_Relationship_4("Formerly engaged or proposed civil partnership"),
-    applicantRespondent_Relationship_5("Live together as a couple"),
-    applicantRespondent_Relationship_6("Formerly lived together as a couple"),
-    applicantRespondent_Relationship_7(
+    appliantRespondent_Relationship_1("Married or in a civil partnership"),
+    appliantRespondent_Relationship_2("Formerly married or in a civil partnership"),
+    appliantRespondent_Relationship_3("Engaged or proposed civil partnership"),
+    appliantRespondent_Relationship_4("Formerly engaged or proposed civil partnership"),
+    appliantRespondent_Relationship_5("Live together as a couple"),
+    appliantRespondent_Relationship_6("Formerly lived together as a couple"),
+    appliantRespondent_Relationship_7(
             "Boyfriend, girlfriend or partner who does not live with me"),
-    applicantRespondent_Relationship_8(
+    appliantRespondent_Relationship_8(
             "Former boyfriend, girlfriend or partner who did not live with me"),
-    applicantRespondent_Relationship_9("None of the above"),
-    applicantRespondent_Relationship_10("Father"),
-    applicantRespondent_Relationship_11("Mother"),
-    applicantRespondent_Relationship_12("Son"),
-    applicantRespondent_Relationship_13("Daughter"),
-    applicantRespondent_Relationship_14("Brother"),
-    applicantRespondent_Relationship_15("Sister"),
-    applicantRespondent_Relationship_16("Grandfather"),
-    applicantRespondent_Relationship_17("Grandmother"),
-    applicantRespondent_Relationship_18("Uncle"),
-    applicantRespondent_Relationship_19("Aunt"),
-    applicantRespondent_Relationship_20("Nephew"),
-    applicantRespondent_Relationship_21("Niece"),
-    applicantRespondent_Relationship_22("Cousin"),
-    applicantRespondent_Relationship_23("Other - please specify");
+    appliantRespondent_Relationship_9("None of the above"),
+    appliantRespondent_Relationship_10("Father"),
+    appliantRespondent_Relationship_11("Mother"),
+    appliantRespondent_Relationship_12("Son"),
+    appliantRespondent_Relationship_13("Daughter"),
+    appliantRespondent_Relationship_14("Brother"),
+    appliantRespondent_Relationship_15("Sister"),
+    appliantRespondent_Relationship_16("Grandfather"),
+    appliantRespondent_Relationship_17("Grandmother"),
+    appliantRespondent_Relationship_18("Uncle"),
+    appliantRespondent_Relationship_19("Aunt"),
+    appliantRespondent_Relationship_20("Nephew"),
+    appliantRespondent_Relationship_21("Niece"),
+    appliantRespondent_Relationship_22("Cousin"),
+    appliantRespondent_Relationship_23("Other - please specify");
 
     private final String relationshipDescription;
 
