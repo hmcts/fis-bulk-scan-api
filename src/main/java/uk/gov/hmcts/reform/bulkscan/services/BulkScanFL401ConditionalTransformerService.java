e037d62ad09d8d2978055397e70bc37f53fd6ad8
bharadwajsairam <bharadwajsairam001@gmail.com>
2025-02-18 16:54:36 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanFL401ConditionalTransformerService.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscan/services/BulkScanFL401ConditionalTransformerService.java
@@ -63,7 +63,13 @@ import static uk.gov.hmcts.reform.bulkscan.constants.BulkScanFl401Constants.TYPE
 
 import com.microsoft.applicationinsights.core.dependencies.google.gson.internal.LinkedTreeMap;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
