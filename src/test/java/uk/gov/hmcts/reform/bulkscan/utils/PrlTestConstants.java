c98368c1375915ca39e2fa471c66762457c06c2d
bharadwajsairam <bharadwajsairam001@gmail.com>
2025-03-21 11:28:14 +0000

    validations added

--- src/test/java/uk/gov/hmcts/reform/bulkscan/utils/PrlTestConstants.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscan/utils/PrlTestConstants.java
@@ -31,22 +31,33 @@ public final class PrlTestConstants {
             "respondentTwoAllAddressesForLastFiveYears";
 
     public static final String INTERNATIONAL_FACTORS_AFFECTING_LITIGATION_WARNING =
-            "Some details are missing in the following section: 8 - Cases with an international"
-                    + " element; 9 - Factors affecting ability to participate in proceedings.";
+            "Some details are missing in the following section: Section 8,9 - "
+                + "atleast 1 required of internationalElement_Resident_of_another_state,"
+                + " internationalElement_jurisdictionIssue, internationalElement_request_toCentral_or_Consular_authority, "
+                + "factorAffectingLitigationCapacity,"
+                + " assessmentByAdultLearningTeam, factorsAffectingPersonInCourt.";
     public static final String EXEMPTION_TO_ATTEND_MIAM_DEPENDENCY_WARNING =
             "Some details are missing in the following section: 2b - Applicant claiming exemption "
-                    + "from the requirement to attend a MIAM.";
+                + "- atleast 1 required - noMIAM_domesticViolance, noMIAM_childProtectionConcerns, noMIAM_Urgency,"
+                + " noMIAM_PreviousAttendence, noMIAM_otherReasons.";
     public static final String NOMIAM_CHILDPROTECTIONCONCERNS_DEPENDENCY_WARNING =
-            "Some details are missing in the following section: 3b - Child protection concerns.";
+            "Some details are missing in the following section: 3b - Child protection concerns - atleast 1 required of "
+                + "noMIAM_subjectOfEnquiries_byLocalAuthority, noMIAM_subjectOfCPP_byLocalAuthority.";
     public static final String NOMIAM_PREVIOUSATTENDENCE_DEPENDENCY_WARNING =
-            "Some details are missing in the following section: 3d - Previous MIAM attendance or"
-                    + " MIAM exemption.";
+            "Some details are missing in the following section: 3d - Previous MIAM attendance or exemption - "
+                + "atleast 1 required of previousMIAM_nonCourtDisputeResolution_4month, "
+                + "previousMIAM_existingProceeding_exepmtMIAM,"
+                + " otherExemption_NoEvidence_reason.";
     public static final String NOMIAM_OTHERREASONS_DEPENDENCY_WARNING =
-            "Some details are missing in the following section: 3e - Other exemptions.";
+            "Some details are missing in the following section: 3e - Other exemptions - atleast 1 required of "
+                + "otherExemption_withoutNotice, otherExemption_byVirture_of_Rule12_3, "
+                + "otherExemption_mediator_notAvailableToConduct, otherExemption_disability_or_inabilityToAttend,"
+                + " otherExemption_NotSufficient_Respondent_ContactDetails, otherExemption_applicant_or_respondent_inPrison.";
     public static final String NOMIAM_URGENCY_DEPENDENCY_WARNING =
-            "Some details are missing in the following section: 3c - Urgency.";
+            "Some details are missing in the following section: 3c - Urgency - atleast 1 required of noMIAM_urgency_{*}.";
     public static final String NOMIAM_DOMESTICVIOLENCE_DEPENDENCY_WARNING =
-            "Some details are missing in the following section: 3a - Domestic violence evidence.";
+            "Some details are missing in the following section: 3a - Domestic violence evidence - "
+                + "atleast 1 required of noMIAM_DVE_{*}.";
     public static final String FL401A_APPLICANT_FULL_NAME_FIELD = "applicant_full_name";
     public static final String FL401A_APPLICANT_ADDRESS_FIELD = "applicant_address";
     public static final String FL401A_APPLICANT_POSTCODE_FIELD = "applicant_postcode";
