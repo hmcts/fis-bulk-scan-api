3491f1b528e654064085b0609e50961251212d22
JohnnyEmbleton <john.embleton@justice.gov.uk>
2025-06-25 11:00:54 +0100

    Another attempt

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,3 +1,10 @@
 terraform {
+  required_version = "1.3.6"
   backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.93.0"
+    }
+  }
 }
